4e2057f529f23000173df5b5a4367560101dd195
def-software <jpablojavier.peralta@gmail.com>
2024-09-22 21:47:15 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 //import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/web.jpg'
+//import viteLogo from '/web.jpg'
 import 'animate.css';
 import './App.css'
 
