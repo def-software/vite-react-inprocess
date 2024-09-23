7c1fda8d05a85c8b93d1f47de299b8037b357d50
def-software <jpablojavier.peralta@gmail.com>
2024-09-22 22:06:39 -0300

    Update App.tsx
    quito animate.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 //import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 //import viteLogo from '/web.jpg'
-import 'animate.css';
+//import 'animate.css';
 import './App.css'
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div>                                 
     </>
   )
 }
